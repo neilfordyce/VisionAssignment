10adb5ed9170a3b0944e940e062f9a239d06c42b
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-11-11 13:30:02 +0000

    removed histogram plotting diagnostics

--- ImageMosaic.m
+++ ImageMosaic.m
@@ -24,23 +24,6 @@ O = I;
             targetPatch = I(j-(IMAGE_SIZE-1):j, k-(IMAGE_SIZE-1):k, :);
             subImageIndex = GetMostSimilarImage(targetPatch, imageHists, lbpHists);
             
-            subplot(2, 3, 1)
-            imshow(uint8(images(:, :, :, subImageIndex)))
-            subplot(2, 3, 2)
-            imshow(uint8(targetPatch))
-            subplot(2, 3, 3)
-            title('source')
-            plot(imageHists(subImageIndex, :))
-            
-            subplot(2, 3, 4)
-            title('target')
-            plot(rgbHist(targetPatch))
-            
-            subplot(2, 3, 5)
-            
-            
-            subplot(2, 3, 6)
-            
             %replace part of the output image with the most similar source
             %image
             O(j-(IMAGE_SIZE-1):j, k-(IMAGE_SIZE-1):k, :) = images(:, :, :, subImageIndex);

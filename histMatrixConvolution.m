382a2b94e5a9bc613449408361550b51fa706fcc
yuebai <y_yuebai@sina.com>
2013-11-13 19:44:10 +0000

    update neighbouring tiles

--- histMatrixConvolution.m
+++ histMatrixConvolution.m
@@ -1,8 +1,14 @@
-function [ convolutedMatrix ] = histMatrixConvolution( originalMatrix )
+function [ convolutedMatrix ] = histMatrixConvolution( Matrix )
 %convert the original matrix to a new weighted matrix
 
+%below is a 3d mask
+%mask(:,:,1) = [1 2 1; 2 4 2; 1 2 1];
+%mask(:,:,2) = [2 4 2; 4 16 4; 2 4 2];
+%mask(:,:,3) = [1 2 1; 2 4 2; 1 2 1];
+
+%below is a 2d mask
 mask = [1 2 1; 2 4 2; 1 2 1];
-convolutedMatrix = conv2(matrixBuild(originalMatrix), mask);
+convolutedMatrix = conv2(matrixBuild(Matrix, 20), mask);
 
 end
 
--- matrixBuild.m
+++ matrixBuild.m
@@ -1,9 +1,11 @@
-function [ outMatrix ] = matrixBuild( I )
+function [ outMatrix ] = matrixBuild( I, PATCH_SIZE )
 %Summary of this function goes here
+
+%blow is an attempt of building a 3d matrix which will be convoluted
 for j=PATCH_SIZE:PATCH_SIZE:size(I, 1)
        for k=PATCH_SIZE:PATCH_SIZE:size(I, 2)
             targetPatch = I(j-(PATCH_SIZE-1):j, k-(PATCH_SIZE-1):k, :);
-            outMatrix(j,k) = rgbHist(targetPatch);
+            outMatrix(:, j/PATCH_SIZE, k/PATCH_SIZE) = rgbHist(targetPatch);
        end
 end
 

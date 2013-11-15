dbdc71d75bd5ba0e7a94f1975552efe544a8e020
jiangyao <joshua.j.yao@gmail.com>
2013-11-15 13:33:01 +0000

    Update distance.m

--- distance.m
+++ distance.m
@@ -1,8 +1,10 @@
 function [ distance ] = distance( histA, histB )
-%Calculate the Chi-square distance of two histograms
+%Calculate the distance of two histograms
 %   Detailed explanation goes here
 
-% Below is the Euclidean Distance
+%Euclidean Distance
 %distance = sqrt((1/size(histA, 1)) * nansum((histA - histB).^2));
+
+%Chi-Square Distance
 distance = 0.5 * nansum((histA-histB).^2 ./ (histA+histB));
 end

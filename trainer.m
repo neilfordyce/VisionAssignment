9057eb2fb3d8476a7ea187b5612ac610892baa3b
jiangyao <joshua.j.yao@gmail.com>
2013-11-19 15:07:41 +0000

    Update trainer.m

--- trainer.m
+++ trainer.m
@@ -9,7 +9,7 @@ function [ mdl, trainingSet, Y ] = trainer()
     
     %Make up the vector which specifies the class of each row in the
     %training set
-    Y = [ones(size(manmadeTraining, 1)); ones(size(naturalTrainging, 1)) * -1];
+    Y = [ones(size(manmadeTraining, 1)); ones(size(naturalTraining, 1)) * -1];
     
     %Make the model from the training set
     mdl = ClassificationKNN.fit(trainingSet, Y, 'NumNeighbors', 7);
